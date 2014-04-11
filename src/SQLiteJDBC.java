f383ad5abde86dcfdd248173c394129ffc4e769c
filipsc <filipsc@kth.se>
2014-04-11 16:25:17 +0200

    Added methods to change the relation-tables USEDIN and PRESENTIN. At the moment there are not checks to ensure that the new amounts wont be negative.

--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -60,7 +60,7 @@ public class SQLiteJDBC {
 		}
 		try{	//kitchen table
 			stmt = mainConnection.createStatement();
-			String sql = "CREATE TABLE KTICHEN " +
+			String sql = "CREATE TABLE KITCHEN " +
 	                   "(NAME	TEXT	PRIMARY KEY	NOT NULL)"; 
 			stmt.executeUpdate(sql);
 			stmt.close();
@@ -100,12 +100,81 @@ public class SQLiteJDBC {
 	public void insertIngredient(String name, String unit){
 		try{
 			Statement stmt = mainConnection.createStatement();
-			String sql = "INSERT INTO INGREDIENT (NAME UNIT) " +
+			String sql = "INSERT IGNORE INTO INGREDIENT (NAME UNIT) " +
 					"VALUES ('" + name + "', '" + unit +"')";
 			stmt.executeUpdate(sql);
 			stmt.close();
 		}catch(Exception e){
 			System.out.println("Crashed at inserting ingredient");
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * Insert a new kitchen into the database
+	 * 
+	 * @param kitchenName , the name will be the key for the kitchen 
+	 */
+	public void insertKitchen(String kitchenName){
+		try{
+			Statement stmt = mainConnection.createStatement();
+			String sql = "INSERT IGNORE INTO INGREDIENT (NAME) " +
+					"VALUES ('" + kitchenName + "')";
+			stmt.executeUpdate(sql);
+			stmt.close();
+		}catch(Exception e){
+			System.out.println("Crashed at inserting kitchen");
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * @param kitName, the kitchen to change in
+	 * @param ingName, the ingredient to be changed
+	 * @param changeAmount, the amount to give or take from the storage
+	 */
+	public void changeIngStoreBy(String kitName, String ingName, int changeAmount){
+		int existingAmount = 0;
+		try{	//get the current amount
+			Statement stmt = mainConnection.createStatement();
+			String sql = "SELECT AMOUNT " +
+					"FROM USEDIN " +
+					"WHERE PRESENTIN.INGREDIENTNAME='" + ingName + "' AND PRESENTIN.KITCHENNAME='" + kitName + "'";
+			ResultSet rs = stmt.executeQuery(sql);
+			existingAmount = rs.getInt("AMOUNT");
+		}catch(Exception e){
+			System.out.println("Unable to find that ingredient");
+			System.exit(0);
+		}
+		int newAmount = changeAmount + existingAmount;
+		try{	// enter the updated amount
+			Statement stmt = mainConnection.createStatement();
+			String sql = "UPDATE PRESENTIN " +
+					"SET AMOUNT='" + newAmount +"' " +
+					"WHERE PRESENTIN.NAME='" + ingName + "'";
+			stmt.executeUpdate(sql);
+			stmt.close();
+		}catch(Exception e){
+			System.out.println("Failed to update that ingredient");
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * @param recName, the name of the recipe to be changed
+	 * @param ingName, the name of the ingredient to be changed
+	 * @param changeAmount, the new amount that should be used by the recipe
+	 */
+	public void setIngUsedIn(String recName, String ingName, int newAmount){
+		try{	// enter the updated amount
+			Statement stmt = mainConnection.createStatement();
+			String sql = "UPDATE USEDIN " +
+					"SET AMOUNT='" + newAmount +"' " +
+					"WHERE USEDIN.NAME='" + ingName + "' AND USEDIN.RECIPE ='" + recName + "'";
+			stmt.executeUpdate(sql);
+			stmt.close();
+		}catch(Exception e){
+			System.out.println("Failed to update that ingredient");
 			System.exit(0);
 		}
 	}
