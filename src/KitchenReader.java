b51f97f66a7cfa5d324753ea66cb11a236c70d71
esokol <ieva_sokolova@hotmail.com>
2014-04-11 17:50:39 +0200

    KitchenReader bör bara funka som översättare mellan GUI:t och databas. Lagt till ny klass GUI_Handler som använder Slick2D-biblioteket.

--- src/KitchenReader.java
+++ src/KitchenReader.java
@@ -4,33 +4,21 @@
  *
  */
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+public class KitchenReader{
 
-public class KitchenReader {
+	/**
+	 * Filip, jag tänkte såhär: Main ligger nu i GUI_Handler, 
+	 * KitchenReader är en klass för att handskas med input från GUI:n
+	 * till databasen. Funkar? 
+	 */
+	private SQLiteJDBC kitchendb;
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	
+	public KitchenReader(){
 		SQLiteJDBC kitchendb = new SQLiteJDBC("kitchen");
 	}
 	
-	public class Frame1 extends JFrame
-	{
-		private static final long serialVersionUID = 1L;
-	JPanel pane = new JPanel();
-	  Frame1() // the frame constructor method
-	  {
-	    super("My Simple Frame"); setBounds(100,100,300,100);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    Container con = this.getContentPane(); // inherit main frame
-	    con.add(pane); // add the panel to frame
-	    // customize panel here
-	    // pane.add(someWidget);
-	    setVisible(true); // display this frame
-	  }
-	}
-
 }
