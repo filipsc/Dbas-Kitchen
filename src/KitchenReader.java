eb79332a1641ce7542161950f1c75ca5c272abc3
filipsc <filipsc@kth.se>
2014-06-09 14:17:49 +0200

    A very simple parser has been built.

--- src/KitchenReader.java
+++ src/KitchenReader.java
@@ -1,22 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 /**
  * A parser which also starts the program.
  */
 public class KitchenReader{
-	SQLiteJDBC kitchendb; 
-	
-	/**
-	 * A parser which also starts the program.
-	 */
 
 	public static void main(String[] args){
 		SQLiteJDBC kitchendb = new SQLiteJDBC("mainkitchen");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		//Testing
 		kitchendb.insertKitchen("Kitchen3"); 
 		kitchendb.insertIngredient("DEFAULT", "X", "Kitchen3");
 		
-		 
+		boolean running = true;
+		while(running){
+			String input = "";
+			try{
+				input = reader.readLine();
+			}catch(IOException ie){
+				ie.printStackTrace();
+				System.out.println("Failed at reading Line");
+			}
+			if(input.equals("exit")){
+				running = false;
+				System.out.println("Exiting program, bye!");
+			}else{
+				System.out.println("Expression not recognized.");
+			}
+		}
 		
 	}
 	
