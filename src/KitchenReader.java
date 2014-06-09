b19d756e588b4dc9523e2d96e3892f35f654a12e
filipsc <filipsc@kth.se>
2014-06-09 18:23:50 +0200

    Naive parser matching KitchenHandler done.

--- src/KitchenReader.java
+++ src/KitchenReader.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.lang.Integer;
 
 /**
  * A parser which also starts the program.
@@ -8,13 +9,14 @@ import java.io.IOException;
 public class KitchenReader{
 
 	public static void main(String[] args){
-		String kitName	= "mainkitchen";
-		SQLiteJDBC kitchendb = new SQLiteJDBC(kitName);
+		//String kitName	= "mainkitchen";	//moved to kitchenhandler
+		//SQLiteJDBC kitchendb = new SQLiteJDBC(kitName);	//moved to kitchenhandler
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		KitchenHandler kitchen = new KitchenHandler("mainkitchen");
 		
 		//Testing
-		kitchendb.insertKitchen("Kitchen3"); 
-		kitchendb.insertIngredient("DEFAULT", "X", "Kitchen3");
+		//kitchendb.insertKitchen("Kitchen3"); 
+		//kitchendb.insertIngredient("DEFAULT", "X", "Kitchen3");
 		
 		boolean running = true;
 		while(running){
@@ -33,7 +35,24 @@ public class KitchenReader{
 			if(words[0].equals("exit")){
 				running = false;
 			}else if(words[0].equals("add ingredient")){
-				kitchendb.insertIngredient(words[1], words[2], kitName);
+				kitchen.newIngredient(words[1], words[2]);
+				System.out.println("Ingredient " + words[1] + " added.");
+			}else if(words[0].equals("change stock")){
+				kitchen.changeStock(words[1], new Integer(words[2]).intValue());
+				System.out.println("Stock for" + words[1] + " changed.");
+			}else if(words[0].equals("stock for")){
+				String needed = kitchen.stockForRecipe(words[1]);
+				System.out.println(needed);
+			}else if(words[0].equals("make")){
+				kitchen.makeRecipe(words[1]);
+				System.out.println("Recipe " + words[1] + " made.");
+			}else if(words[0].equals("shop for")){
+				String[] recipes = new String[words.length-2];
+				for(int i = 1; i < words.length; i++){
+					recipes[i - 1] = words[i];
+				}
+				String shoppingList = kitchen.shoppingList(recipes);
+				System.out.println(shoppingList);
 			}else{
 				System.out.println("Expression not recognized.");
 			}
